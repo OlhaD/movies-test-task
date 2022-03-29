b41cde6713d219467daa6a74849075e9a02aded3
Olga <olgadorogko@gmail.com>
2022-03-28 20:42:02 -0400

    Update components structure

--- movies/src/app/movies/movies.module.ts
+++ movies/src/app/movies/movies.module.ts
@@ -4,12 +4,16 @@ import { MoviesRoutingModule } from './movies-routing.module';
 import { MoviesComponent } from './components/movies/movies.component';
 import { MoviesListPageComponent } from './pages/movies-list-page/movies-list-page.component';
 import { MoviesBookmarkedPageComponent } from './pages/movies-bookmarked-page/movies-bookmarked-page.component';
+import { MovieCardComponent } from './components/movie-card/movie-card.component';
+import { SearchFormComponent } from './components/search-form/search-form.component';
 
 @NgModule({
   declarations: [
     MoviesComponent,
     MoviesListPageComponent,
-    MoviesBookmarkedPageComponent
+    MoviesBookmarkedPageComponent,
+    MovieCardComponent,
+    SearchFormComponent
   ],
   imports: [
     MoviesRoutingModule
