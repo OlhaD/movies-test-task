6af5e2e9d862d821c549636210bb34ddb90310bb
Olga <olgadorogko@gmail.com>
2022-03-31 14:27:34 -0400

    Add routing link to bookmarked movies page and back

--- movies/src/app/movies/components/movies/movies.component.ts
+++ movies/src/app/movies/components/movies/movies.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
-  styleUrls: ['./movies.component.scss']
+  styleUrls: ['./movies.component.scss'],
 })
 export class MoviesComponent implements OnInit {
+  isHomePage: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private location: Location,
+    private router: Router
+  ) {
+    router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isHomePage = this.location.path() === '';
+      }
+    });
   }
 
+  ngOnInit(): void {}
 }
